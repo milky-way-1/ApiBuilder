0f8263b05fbfd22cf165f2e5d0524e04decd1aa0
milky-way-1 <shettysuhassc@gmail.com>
2025-01-15 01:43:41 +0530

    Update FormResponseRepository.java

--- src/main/java/com/apibuilder/dev/apibuilder/repository/FormResponseRepository.java
+++ src/main/java/com/apibuilder/dev/apibuilder/repository/FormResponseRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface FormResponseRepository extends MongoRepository<FormResponse, String> {
     Page<FormResponse> findByFormTemplateId(String formTemplateId, Pageable pageable);
     Page<FormResponse> findBySubmittedBy(String userId, Pageable pageable);
+    List<FormResponse> findByFormTemplateId(String formTemplateId);
 }
