645abeb8a4770910d263276fe75df0442a090b73
milky-way-1 <shettysuhassc@gmail.com>
2025-01-14 11:38:25 +0530

    Update FormFieldResponse.java

--- src/main/java/com/apibuilder/dev/apibuilder/dto/response/FormFieldResponse.java
+++ src/main/java/com/apibuilder/dev/apibuilder/dto/response/FormFieldResponse.java
@@ -1,5 +1,7 @@
 package com.apibuilder.dev.apibuilder.dto.response;
 import com.apibuilder.dev.apibuilder.constant.FieldType;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.util.List;
@@ -9,9 +11,14 @@ import java.util.List;
 public class FormFieldResponse {
     private String fieldId;
     private String name;
-    private FieldType type;
-    private boolean isUnique;
-    private boolean isMandatory;
+    private FieldType type;  
+    
+    @JsonProperty("isUnique")
+    private boolean isUnique; 
+    
+    @JsonProperty("isMandatory")
+    private boolean isMandatory; 
+    
     private String validations;
     private boolean longText;
     private String placeholder;
